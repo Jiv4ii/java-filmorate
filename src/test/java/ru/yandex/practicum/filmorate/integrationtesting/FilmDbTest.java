66050cb5fb35c6571327a4646905775700075889
Jiv4ii <usaaalee@gmail.com>
2023-10-03 21:18:53 +0500

    Не понял проблему

--- src/test/java/ru/yandex/practicum/filmorate/integrationtesting/FilmDbTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/integrationtesting/FilmDbTest.java
@@ -29,17 +29,19 @@ public class FilmDbTest {
 
     @Test
     void testFilmDb() {
-        Film film = new Film().setId(1).setName("film").setDescription("descr").setDuration(120).setReleaseDate(LocalDate.of(2000, 1, 1)).setMpa(new Rating().setId(2L).setName("PG"));
-        Film film2 = new Film().setId(2).setName("film2").setDescription("descr2").setDuration(120).setReleaseDate(LocalDate.of(2000, 1, 1)).setMpa(new Rating().setId(2L).setName("PG"));
-        Film film3 = new Film().setId(3).setName("film3").setDescription("descr3").setDuration(120).setReleaseDate(LocalDate.of(2000, 1, 1)).setMpa(new Rating().setId(2L).setName("PG"));
+        Film film = new Film().setName("film").setDescription("descr").setDuration(120).setReleaseDate(LocalDate.of(2000, 1, 1)).setMpa(new Rating().setId(2L).setName("PG"));
+        Film film2 = new Film().setName("film2").setDescription("descr2").setDuration(120).setReleaseDate(LocalDate.of(2000, 1, 1)).setMpa(new Rating().setId(2L).setName("PG"));
+        Film film3 = new Film().setName("film3").setDescription("descr3").setDuration(120).setReleaseDate(LocalDate.of(2000, 1, 1)).setMpa(new Rating().setId(2L).setName("PG"));
         Film updatedFilm = new Film().setId(1).setName("upfilm").setDescription("updescr").setDuration(120).setReleaseDate(LocalDate.of(2000, 1, 1)).setMpa(new Rating().setId(2L).setName("PG"));
-        User user = new User().setId(1).setEmail("email1@stas.ru").setName("Name").setLogin("login1stas").setBirthday(LocalDate.of(2000, 1, 1));
-        User user2 = new User().setId(2).setEmail("email2@fred.ru").setName("Name2").setLogin("login2fred").setBirthday(LocalDate.of(2000, 1, 1));
-        User user3 = new User().setId(3).setEmail("email3@jorsh.ru").setName("Name3").setLogin("login3jorsh").setBirthday(LocalDate.of(2000, 1, 1));
+        User user = new User().setEmail("email1@stas.ru").setName("Name").setLogin("login1stas").setBirthday(LocalDate.of(2000, 1, 1));
+        User user2 = new User().setEmail("email2@fred.ru").setName("Name2").setLogin("login2fred").setBirthday(LocalDate.of(2000, 1, 1));
+        User user3 = new User().setEmail("email3@jorsh.ru").setName("Name3").setLogin("login3jorsh").setBirthday(LocalDate.of(2000, 1, 1));
         userDbStorage.addUser(user);
         userDbStorage.addUser(user2);
         userDbStorage.addUser(user3);
-
+        System.out.println(userDbStorage.getUser(1));
+        System.out.println(userDbStorage.getUser(2));
+        System.out.println(userDbStorage.getUser(3));
         filmDbStorage.addFilm(film);
         Assertions.assertEquals(film, filmDbStorage.getFilm(1), "Неверное сохраненения фильма в базу");
 
@@ -59,9 +61,6 @@ public class FilmDbTest {
         filmDbStorage.addLike(1, 1);
         filmDbStorage.addLike(3, 2);
         filmDbStorage.addLike(3, 1);
-        System.out.println(userDbStorage.getUser(1));
-        System.out.println(userDbStorage.getUser(2));
-        System.out.println(userDbStorage.getUser(3));
 
         List<Film> films = new ArrayList<>();
         films.add(film);
