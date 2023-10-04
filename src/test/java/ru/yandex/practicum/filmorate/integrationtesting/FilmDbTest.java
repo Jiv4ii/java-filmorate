0c2a4e4a54aa578222876813b7600f55c3c2b392
Jiv4ii <usaaalee@gmail.com>
2023-10-04 16:22:27 +0500

    Заплатки

--- src/test/java/ru/yandex/practicum/filmorate/integrationtesting/FilmDbTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/integrationtesting/FilmDbTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import ru.yandex.practicum.filmorate.model.Film;
@@ -25,7 +24,6 @@ import java.util.Set;
 @SpringBootTest
 @AutoConfigureTestDatabase
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class FilmDbTest {
 
     private final FilmDbStorage filmDbStorage;
